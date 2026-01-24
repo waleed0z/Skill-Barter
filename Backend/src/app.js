e0080ee3d200f9e94064e21422010d72e5dc7f5f
Olalekan Macaulay <olalekan.macaulay1@gmail.com>
2026-01-24 17:57:19 +0100

    Add Swagger API documentation

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -2,9 +2,12 @@ const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
 const morgan = require('morgan');
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
 require('dotenv').config();
 
 const authRoutes = require('./routes/auth.routes');
+const swaggerDocument = YAML.load('./src/docs/swagger.yaml');
 
 const app = express();
 
@@ -15,6 +18,7 @@ app.use(express.json());
 app.use(morgan('dev'));
 
 // Routes
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use('/auth', authRoutes);
 
 app.get('/', (req, res) => {
